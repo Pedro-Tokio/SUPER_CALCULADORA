336e6562197c6d30f329fcc9b9897edc7bd39c7d
Pedro-Tokio <PedroTokio.Muta@gmail.com>
2020-07-08 13:50:48 -0300

    Update SUPER_CALCULADORA.cpp
    Arrumei a equação do 2°grau e adicionei a opção de exibir uma tabuada completa

--- SUPER_CALCULADORA.cpp
+++ SUPER_CALCULADORA.cpp
@@ -16,7 +16,7 @@ main(){
 	printf("-------------------------\n");
 	
 	do{
-		printf("\nEscolha um dos operadores: \n[1]Soma \n[2]Subtra��o \n[3]Muntiplica��o \n[4]Divis�o \n[5]Equação do 2° grau \n-->");
+		printf("\nEscolha um dos operadores: \n[1]Soma \n[2]Subtração \n[3]Muntiplicação \n[4]Divisão \n[5]Equação do 2° grau \n-->");
 		scanf("%d", &e);
 	
 		switch(e){
@@ -39,12 +39,35 @@ main(){
 				break;
 				
 			case 3:
-				printf("\nDigite o primeiro valor: \n");
-				scanf("%d", &n1);
-				printf("\nDigite o segundo valor: \n");
-				scanf("%d", &n2);
-				resul = n1 * n2;
-				printf("\nRESULTADO: \n%d", resul);
+				int n, t;
+				
+				printf("Tabuada completa ou calculo único? \n[1]Tabuada completa \n[Calculo único] \n -- >");
+				scanf("%d", &n);
+				
+				if (n == 1){
+					printf("Digite o número que deseja multiplicar: \n-->");
+					scanf("%d", &n1);
+					printf("Até que número o multiplicador irá? \n-->");
+					scanf("%d", &n2);
+		
+					while(i < n2){
+						i = i + 1;
+						t = n1 * i;
+						printf("%d X %d = %d \n", n1, i, t);
+					}
+				}
+				
+				if (n == 2){
+				
+				
+					printf("\nDigite o primeiro valor: \n");
+					scanf("%d", &n1);
+					printf("\nDigite o segundo valor: \n");
+					scanf("%d", &n2);
+					resul = n1 * n2;
+					printf("\nRESULTADO: \n%d", resul);
+				}
+				
 				break;
 				
 			case 4:
@@ -54,61 +77,53 @@ main(){
 				scanf("%d", &n2);
 				while (n2 == 0)
 				{
-					printf("\n0 n�o � um divisor v�lido. \nDigite outro valor: \n");
+					printf("\n0 não é um divisor válido. \nDigite outro valor: \n");
 					scanf("%d", &n2);
 				}
 				resul = n1 / n2;
 				printf("\nRESULTADO: \n%d", resul);
 				break;
-
+				
 			case 5:
 				int a, b, c, delta, x1, x2;
-				printf("\nDigite a valor de A: \n");
+				printf("\nDigite o valor de A: \n");
 				scanf("%d", &a);
 				printf("\nDigite o valor de B: \n");
 				scanf("%d", &b);
 				printf("\nDigite o valor de C: \n");
 				scanf("%d", &c);
-
+				
 				delta = pow(b, 2) - 4 * a * c;
 				printf("\nDelta = %d", delta);
-				if (delta > 0)
-				{
+				if (delta > 0){
 					x1 = (-b + sqrt(delta)) / (2 * a);
 					x2 = (-b - sqrt(delta)) / (2 * a);
 					printf("\nX1 = %d", x1);
 					printf("\nX2 = %d", x2);
-				}
-
-				if (delta == 0)
-				{
+					}
+					
+				if (delta == 0){
 					printf("\nDelta é igual a 0, então só possui uma raiz real. \n");
 					x1 = -b / (2 * a);
-					printf("\nX = %d", x1);
-				}
-
-				else
-				{
+					printf("X = %d", x1);
+					}
+				
+				if (delta < 0){
 					printf("\nNão possui raiz real para X");
-				}
-				break;
-				
-				
-				
-			default:
-				printf("Valor inv�lido!");
+					}
+				break;			
+				default:
+					printf("Valor inválido!");
 				
 		}
 	
 		
-		printf("\n\nDeseja continuar? \n[1]SIM \n[2]N�O \n-->");
+		printf("\n\nDeseja continuar? \n[1]SIM \n[2]NÃO \n-->");
 		scanf("%d", &i);
 		while (i != 1 && i != 2){
-			printf("Op��o inv�lida! \n-->");
+			printf("Opção inválida! \n-->");
 			scanf("%d", &i);
 		}
 	}while(i == 1);
 }
-	
-	
 	
